920b74d36533a79093b603cbae49be15bf45942d
Saulo <saulo.scientist@gmail.com>
2025-08-18 10:07:58 -0300

    Calculadora de Preço

---
+++ preco.py
@@ -0,0 +1,13 @@
+# Informações do produto
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+# Cálculo do preço total
+preco_total = preco_unitario * quantidade
+
+# Exibição das informações
+print("Produto:", nome_produto)
+print("Preço unitário: R$ {:.2f}".format(preco_unitario))
+print("Quantidade:", quantidade)
+print("Preço total: R$ {:.2f}".format(preco_total))
