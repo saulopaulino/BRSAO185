81f36f99bed5ae7a1365069a344de84aa388342a
Saulo <saulo.scientist@gmail.com>
2025-08-18 09:57:30 -0300

    Calculadora de Volume

---
+++ volume.py
@@ -0,0 +1,10 @@
+# Dimensões (em cm)
+comprimento = 12
+largura = 14
+altura = 20
+
+# Cálculo do volume
+volume = comprimento * largura * altura
+
+# Resultado
+print(f"O volume da caixa é {volume} cm³")
