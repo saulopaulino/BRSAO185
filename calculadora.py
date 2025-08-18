df59155ec915934e4d574c02f494777f59c1d68b
Saulo <saulo.scientist@gmail.com>
2025-08-18 10:14:19 -0300

    Calculadora de Número Inteiro

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+#Variáveis
+
+A = int(input("Digite o primeiro número: "))
+B = int(input("Digite o segundo número: "))
+C = int(input("Digite o terceiro número: "))
+D = int(input("Digite o quarto número: "))
+
+#Cálculo
+
+resultado = (A * B - C * D)
+
+#Exibição do resultado
+print("A DIFERENÇA é:", resultado)
